47c060d38d5be22544e8a23cb134d04aebeeabed
Akshay Patra <akshay.patra114@gmail.com>
2024-06-30 11:50:53 +0530

    designed the login page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Signup from './pages/Signup';
+import LoginPage from './pages/LoginPage';
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
         <Routes>
             <Route exact path="/" element={<Home/>} />
             <Route  path="/signup" element={<Signup/>} />
+            <Route  path="/login" element={<LoginPage/>} />
             
         </Routes>
 
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function LoginPage() {
+  return (
+    <div className='LoginPage'>
+        <div className='form-container '>
+        <h4 className='header my-2'>Log In to College Community</h4>
+            <form >
+                <div className='container my-5'>
+                
+                
+                
+                
+                
+                
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email address" required></input>
+                <input id='password' type='password' className='form-control my-2' placeholder='Password' required></input>
+                </div>
+
+                <div>
+                <button className='Signup-button'>Submit</button>
+                <button className='Login-button'>Reset</button>
+                </div>
+
+                Don't have an account ? <Link className='login-link'  to="/signup">SignUp</Link>
+            </form>
+
+        </div>
+      
+    </div>
+  )
+}
