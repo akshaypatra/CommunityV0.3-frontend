ff7b3e41175154e953cda7582727d903fccd7feb
Akshay Patra <akshay.patra114@gmail.com>
2024-08-05 14:33:08 +0530

    used api endpoints to send the sigunp and login details to the server

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,26 +1,57 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { useState } from 'react'
+import { Link,useNavigate  } from 'react-router-dom'
+import axios
+ from 'axios';
 export default function LoginPage() {
+
+  const [post,setPost]=useState({
+    username:" ",
+    password:" "
+  });
+
+  const handleInput=(event)=>{
+    setPost({...post,[event.target.name]: event.target.value});
+  }
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('http://127.0.0.1:8000/api/api-token-auth/', post)
+      .then(response => {
+        
+        const { token } = response.data;
+      
+        localStorage.setItem('authToken', token);
+        // Redirect to home page
+        navigate('/');
+      })
+      .catch(err => {
+        console.error('Error during login:', err.response ? err.response.data : err.message);
+      });
+  };
+
+
+  const handleReset = () => {
+    setPost({
+      username:" ",
+      password:" "
+    });
+  };
+
   return (
     <div className='LoginPage'>
         <div className='form-container '>
         <h4 className='header my-2'>Log In to College Community</h4>
-            <form >
+            <form onSubmit={handleSubmit} >
                 <div className='container my-5'>
-                
-                
-                
-                
-                
-                
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email address" required></input>
-                <input id='password' type='password' className='form-control my-2' placeholder='Password' required></input>
+                <input type="email" class="form-control" id="username" placeholder="Email address" name='username' onChange={handleInput} required></input>
+                <input id='password' type='password' className='form-control my-2' placeholder='Password' name='password' onChange={handleInput} required></input>
                 </div>
 
                 <div>
-                <button className='Signup-button'>Submit</button>
-                <button className='Login-button'>Reset</button>
+                <button className='Signup-button' type='submit'>Submit</button>
+                <button className='Login-button' onClick={handleReset}>Reset</button>
                 </div>
 
                 Don't have an account ?  <Link className='login-link'  to="/signup">SignUp</Link>
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,49 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+
 
 export default function Signup() {
+
+  const [post,setPost]=useState({
+
+    first_name:" ",
+    last_name:" ",
+    college_name:" ",
+    state:" ",
+    city:" ",
+    enrollment_number:" ",
+    department:" ",
+    academic_year:" ",
+    email:" ",
+    password:" ",
+
+  })
+
+  const handleInput=(event)=>{
+      setPost({...post,[event.target.name]: event.target.value});
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('http://127.0.0.1:8000/api/register/',post)
+    .then(response => console.log(response))
+    .catch(err =>{console.error("Error during signup:", err.response ? err.response.data : err.message);})
+  }
+
+  const handleReset = () => {
+    setPost({
+      first_name: "",
+      last_name: "",
+      college_name: "",
+      state: "",
+      city: "",
+      enrollment_number: "",
+      department: "",
+      academic_year: "",
+      email: "",
+      password: "",
+      confirm_password: ""
+    });
+  };
+
   return (
     <div className='signupPage'>
         <h2 className='my-3'>Welcome to College Community</h2><br/>
         
         <div className='form-container'>
         <h4 className='header my-2'>Enter your details to create an account</h4>
-            <form >
+            <form onSubmit={handleSubmit} >
                 <div className='container my-5'>
                 
-                <input id='firstname' className='form-control my-2' placeholder='First Name' required/>
+                <input id='first_name' className='form-control my-2' onChange={handleInput} placeholder='First Name' name='first_name'  required/>
                 
-                <input id='lastname' className='form-control my-2' placeholder='Last Name' required></input>
-                <input id='CollegeName' className='form-control my-2' placeholder='College Name' required></input>
-                <input id='State' className='form-control my-2' placeholder='State' required></input>
-                <input id='City' className='form-control my-2' placeholder='City' required></input>
-                <input id='enrollmentNumber' className='form-control my-2' placeholder='Enrollment Number' required ></input>
-                <select class="form-select" aria-label="Default select example" required>
-                        <option selected>Select Department</option>
-                        <option value="1">Btech CSE core</option>
-                        <option value="2">Btech IT</option>
-                        <option value="3">Btech Artificial Intelligence </option>
-                        <option value="4">Btech Cloud Computing </option>
-                        <option value="5">Btech AI/ML </option>
-                        <option value="6">Btech Blockchain </option>
-                        <option value="7">Btech Cyber Security </option>
+                <input id='last_name' className='form-control my-2' onChange={handleInput} placeholder='Last Name' name='last_name'  required></input>
+                <input id='college_name' className='form-control my-2' onChange={handleInput} placeholder='College Name' name='college_name'  required></input>
+                <input id='state' className='form-control my-2' onChange={handleInput} placeholder='State'  name='state'  required></input>
+                <input id='city' className='form-control my-2' onChange={handleInput} placeholder='City' name='city'  required></input>
+                <input id='enrollment_number' className='form-control my-2' onChange={handleInput} placeholder='Enrollment Number'   name='enrollment_number' required ></input>
+                <select id='department' className="form-select" onChange={handleInput} aria-label="Default select example" value={post.department} name='department' required>
+                        <option value="">Select Department</option>
+                        <option value="Btech CSE core">Btech CSE core</option>
+                        <option value="Btech IT">Btech IT</option>
+                        <option value="Btech Artificial Intelligence ">Btech Artificial Intelligence </option>
+                        <option value="Btech Cloud Computing">Btech Cloud Computing </option>
+                        <option value="Btech AI/ML">Btech AI/ML </option>
+                        <option value="Btech Blockchain">Btech Blockchain </option>
+                        <option value="Btech Cyber Security">Btech Cyber Security </option>
                 </select>
-                <select class="form-select my-2" aria-label="Default select example" required>
-                        <option selected>Academic Year</option>
+                <select id='academic_year' className="form-select my-2" onChange={handleInput} aria-label="Default select example"  name='academic_year' value={post.academic_year} required>
+                        <option value="">Academic Year</option>
                         <option value="1">First year</option>
                         <option value="2">Second year</option>
                         <option value="3">Third year </option>
                         <option value="4">Final year </option>
                         
                 </select>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email address" required></input>
-                <input id='password' type='password' className='form-control my-2' placeholder='Set Password' required></input>
-                <input id='Confirmpassword' type='password' className='form-control my-2' placeholder='Confirm Password' required ></input>
+                <input type="email" className="form-control" id="email" onChange={handleInput} placeholder="Email address"  name="email"  required></input>
+                <input id='password' type='password' className='form-control my-2' onChange={handleInput} placeholder='Set Password'  name='password'   required></input>
+            
                 </div>
 
                 <div>
-                <button className='Signup-button'>Create Account</button>
-                <button className='Login-button'>Reset</button>
+                <button className='Signup-button' type='submit'>Create Account</button>
+                <button className='Login-button' type='button' onClick={handleReset}>Reset</button>
                 </div>
 
                 Already have an account ? <Link className='login-link'  to="/login"  >Login</Link>
@@ -54,3 +100,6 @@ export default function Signup() {
     </div>
   )
 }
+
+
+// registration endpoint : http://127.0.0.1:8000/api/register/
